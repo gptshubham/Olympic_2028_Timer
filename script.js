20f3e9a214f713c2c314ca8317518934270881af
gptshubham <shubhamca76@gmail.com>
2024-12-19 20:03:51 +0530

    header styles added

--- script.js
+++ script.js
@@ -27,3 +27,67 @@ function displayTime() {
 }
 
 setInterval(displayTime, 1000);
+
+// Styling the page using DOM Manipulation
+
+// body styles
+const body = document.body;
+body.style.color = 'white';
+body.style.margin = '0';
+body.style.height = '100vh';
+body.style.fontFamily = 'helvetica, sans-serif';
+body.style.background =
+  'linear-gradient(60deg, #333 0%,rgb(34, 132, 141) 30%,rgb(166, 167, 171) 60%,rgb(196, 25, 196) 100%)';
+
+// header styles
+const header = document.querySelector('#head');
+header.style.display = 'flex';
+header.style.justifyContent = 'space-between';
+header.style.alignItems = 'center';
+header.style.padding = '5px 10px';
+header.style.background = 'linear-gradient(to bottom, #333, #555)';
+
+// search bar styles
+const searchBox = document.querySelector('#search-box');
+searchBox.style.backgroundColor = 'white';
+searchBox.style.color = 'black';
+searchBox.style.padding = '5px';
+searchBox.style.borderRadius = '3px';
+searchBox.style.height = '20px';
+
+const inputTag = document.querySelector('input');
+inputTag.style.border = 'none';
+inputTag.style.height = '20px';
+
+const searchBoxLine = document.querySelector('#search-box-line');
+searchBoxLine.style.color = 'rgb(166, 167, 171)';
+
+// first div styles
+const firstDiv = document.querySelector('div');
+firstDiv.style.display = 'flex';
+firstDiv.style.justifyContent = 'center';
+firstDiv.style.alignItems = 'center';
+
+// logo styles
+const logo = document.querySelector('#logo');
+logo.style.margin = '0';
+logo.style.fontWeight = '100';
+
+const lastLetterOfLogo = document.querySelector('#logo-o');
+lastLetterOfLogo.style.color = 'red';
+
+// dropdown styles
+
+const dropdown = document.querySelectorAll('.drop');
+
+for (let item of dropdown) {
+  item.style.paddingLeft = '20px';
+}
+
+const selections = document.querySelectorAll('select');
+
+for (let option of selections) {
+  option.style.background = 'inherit';
+  option.style.border = 'none';
+  option.style.color = 'white';
+}
